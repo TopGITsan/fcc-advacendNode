ef03f971b220f8744f636b5c9c5c28e576c689cc
TopGitSan <c.toparcean@gmail.com>
2019-05-08 19:56:53 +0200

    add and use express-session

--- server.js
+++ server.js
@@ -2,16 +2,26 @@
 
 const express     = require('express');
 const bodyParser  = require('body-parser');
+const mongodb     = require('mongodb');
+const session     = require('express-session');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
 app.set('view engine','pug');
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 app.route('/')
   .get((req, res) => {
